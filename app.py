74ad1980b5699a2fb34df6c17d9daf5536d16c1a
singharaj usai <djjd22938@gmail.com>
2023-10-14 17:05:29 -0400

    project 2 hint wip

--- app.py
+++ app.py
@@ -7,3 +7,10 @@
 # roll a 1, lose points from this turn
 
 # Roll double 1s, lose all points
+
+import random
+
+def roll_dice():
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    return dice1, dice2
