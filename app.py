2d99b5dca344b6f85f3ee0a51855569f811e0be7
singharaj usai <djjd22938@gmail.com>
2023-10-15 01:50:35 -0400

    single 1 roll

--- app.py
+++ app.py
@@ -16,12 +16,18 @@ def roll_dice():
     return dice1, dice2
 
 # step 2
-
+# computer_turn(game_score, goal) - performs the computer’s turn. Computer is provided a goal to achieve and then stop rolling for the current turn.
 def computerTurn(game_score, goal):
+    # Turn total starts at zero.
     turnTotal = 0
     while True:
         dice1, dice2 = roll_dice()
         print(f"computer rolled {dice1}, {dice2} turn total: {turnTotal}")
+
+        # If a single 1 is rolled, set the current turn score to zero and end the turn.
+        if dice1 == 1 or dice2 == 1:
+            return game_score
+
 
 if __name__ == "__main__":
     print(roll_dice())
